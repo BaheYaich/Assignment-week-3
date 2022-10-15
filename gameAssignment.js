aed20b32189a3e357ce11eb169f9f95e771e4d8d
Bahe Yaich <Bahe.yaich@live.ca>
2022-10-15 19:39:55 +0300

    Loop fix

--- gameAssignment.js
+++ gameAssignment.js
@@ -7,6 +7,7 @@ let score = 0;
 let wins = 0;
 let losses = 0;
 let gameOutcome;
+let gameRounds = true;
 
 // Computer logic
 function computerPlay() {
@@ -26,15 +27,15 @@ function gameRound (playerSelection, computerSelection) {
         case 'rockscissors':
         case 'paperrock':
             gameOutcome = 'YOU WIN! You chose ' + playerSelection + ' and the computer chose ' + computerSelection
-            score++;
-            wins++;
+            score += 1;
+            wins += 1;
             break
         case 'paperscissors':
         case 'scissorsrock':
         case 'rockpaper':
             gameOutcome = 'YOU LOSE! You chose ' + playerSelection + ' and the computer chose ' + computerSelection
-            score--;
-            losses++;
+            score -= 1;
+            losses += 1;
             break
         case 'scissorsscissors':
         case 'rockrock':
@@ -47,38 +48,27 @@ function gameRound (playerSelection, computerSelection) {
     return gameOutcome;
 };
 
-function bestOfFive (wins, draws, losses){
-    switch (score) {
-        case (score === 0) :
-            console.log("Draw, It's inconclusive");
-            break;
-        case (score > 0) :
-            console.log("You won the best of five");
-            break;
-        case (score < 0) :
-            console.log("You lost the best of five");
-            break;
-    }
-};
-
 function game() {
-    let gameRounds = true;
-    while (gameRounds) {
-        for (let i = 0; i < 5; i++) {
-            gameRound(); 
-            console.log(gameOutcome);
-            if (wins >= 3) {
-                gameRounds = !gameRounds;
-                break;
-            }
-            else if (losses >= 3){
-                gameRounds = !gameRounds;
-                break;
-            }
-        } 
-        gameRounds = !gameRounds;
-        bestOfFive();   
+    for (let i = 0; i < 5; i++) {
+        gameRound(); 
+        // Best of five mechanic
+        if (wins === 3) {
+            break
+        }
+        else if (losses === 3){
+            break
+        }
     }
-}
+    // Score decides the game's outcome
+    if (score === 0) {
+        console.log("Draw, It's inconclusive")
+    }
+    else if (score > 0) {
+        console.log("You won the best of five")
+    }      
+    else if (score < 0) {
+        console.log("You lost the best of five")
+    }    
+};
 
 game();
